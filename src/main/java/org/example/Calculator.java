fb02c7b3c8f131f0b16cad2367902413f74ae766
sukrutdekhane <dekhanesd7@gmail.com>
2023-03-14 14:44:56 +0530

    Added factorial functionality.

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        Scanner scanner = new Scanner(System.in);
+        double number1, number2;
+
+        do {
+            System.out.println("Scientific Calculator using DevOps. \n Choose operation:");
+            System.out.print("1. Factorial\n2. Square root\n3. Power\n4. Natural Logarithm\n" +
+                    "5. Exit\nEnter your choice: ");
+            int choice;
+            try {
+                choice = scanner.nextInt();
+            } catch (InputMismatchException error) {
+                return;
+            }
+
+            switch (choice) {
+                case 1:
+                    // Factorial
+                    System.out.print("Enter a number : ");
+                    number1 = scanner.nextDouble();
+                    System.out.println("Factorial of "+number1+" is : " + calculator.factorial(number1));
+                    System.out.println("\n");
+
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default:
+
+                    return;
+            }
+        } while (true);
+    }
+
+    public double factorial(double number1){
+        double result = fact(number1);
+        return result;
+    }
+
+    public double fact(double num) {
+        double facto = 1;
+        for(int i = 1; i <= num; ++i)
+        { facto *= i;   }
+        return  facto;
+    }
+}
