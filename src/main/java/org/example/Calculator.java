1611a52a669f09bfc583397fae1382d247fc2362
sukrutdekhane <dekhanesd7@gmail.com>
2023-03-15 16:02:34 +0530

    Added power functionality.

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -36,6 +36,12 @@ public class Calculator {
                     System.out.println("\n");
                     break;
                 case 3:
+                    System.out.print("Enter the first number : ");
+                    number1 = scanner.nextDouble();
+                    System.out.print("Enter the second number : ");
+                    number2 = scanner.nextDouble();
+                    System.out.println(number1+ " raised to power "+number2+" is : " + calculator.power(number1, number2));
+                    System.out.println("\n");
                     break;
                 case 4:
                     break;
@@ -61,6 +67,10 @@ public class Calculator {
     }
     public double squareRoot(double number1) {
         double result = Math.sqrt(number1);
+        return result;
+    }
+    public double power(double number1, double number2) {
+        double result = Math.pow(number1,number2);
         return result;
     }
 }
