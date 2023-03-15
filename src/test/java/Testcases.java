79489e2fe67407334113f5e504b8192dbd9dee32
sukrutdekhane <dekhanesd7@gmail.com>
2023-03-15 16:10:00 +0530

    Added power test cases.

--- src/test/java/Testcases.java
+++ src/test/java/Testcases.java
@@ -24,4 +24,21 @@ public class Testcases {
         assertNotEquals("Finding factorial of a number for False Positive", 9, calculator.fact(2), DELTA);
         assertNotEquals("Finding factorial of a number for False Positive", 0, calculator.fact(0), DELTA);
     }
+
+    @Test
+    public void powerTruePositive(){
+        assertEquals("Finding power for True Positive", 8, calculator.power(2, 3), DELTA);
+        assertEquals("Finding power for True Positive", 1, calculator.power(1, 3), DELTA);
+        assertEquals("Finding power for True Positive", 81, calculator.power(3, 4), DELTA);
+        assertEquals("Finding power for True Positive", 64, calculator.power(4, 3), DELTA);
+        assertEquals("Finding power for True Positive", 25, calculator.power(5, 2), DELTA);
+    }
+
+    @Test
+    public void powerFalsePositive(){
+        assertNotEquals("Finding power for False Positive", 6, calculator.power(2, 3), DELTA);
+        assertNotEquals("Finding power for False Positive", -7, calculator.power(3, 3), DELTA);
+        assertNotEquals("Finding power for False Positive", 99, calculator.power(4, 3), DELTA);
+        assertNotEquals("Finding power for False Positive", -12, calculator.power(5, 3), DELTA);
+    }
 }
