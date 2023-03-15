d00bf014d39718f31063c58e1dd01427e84e3730
sukrutdekhane <dekhanesd7@gmail.com>
2023-03-15 15:05:28 +0530

    Added test cases for factorial function.

---
+++ src/test/java/testcases.java
@@ -0,0 +1,27 @@
+import org.example.Calculator;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class testcases {
+    private static final double DELTA = 1e-15;
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void factorialTruePositive(){
+        assertEquals("Finding factorial of a number for True Positive", 720, calculator.fact(6), DELTA);
+        assertEquals("Finding factorial of a number for True Positive", 1, calculator.fact(1), DELTA);
+        assertEquals("Finding factorial of a number for True Positive", 6, calculator.fact(3), DELTA);
+        assertEquals("Finding factorial of a number for True Positive", 24, calculator.fact(4), DELTA);
+        assertEquals("Finding factorial of a number for True Positive", 1, calculator.fact(0), DELTA);
+    }
+
+    @Test
+    public void factorialFalsePositive(){
+        assertNotEquals("Finding factorial of a number for False Positive", 113, calculator.fact(5), DELTA);
+        assertNotEquals("Finding factorial of a number for False Positive", 10, calculator.fact(6), DELTA);
+        assertNotEquals("Finding factorial of a number for False Positive", 42, calculator.fact(4), DELTA);
+        assertNotEquals("Finding factorial of a number for False Positive", 9, calculator.fact(2), DELTA);
+        assertNotEquals("Finding factorial of a number for False Positive", 0, calculator.fact(0), DELTA);
+    }
+}
